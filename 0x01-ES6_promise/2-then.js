4c6213d0ba47f5f965b7f7162cea9ee819004484
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-06-04 22:30:39 +0300

    task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,10 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({ body: 'Success', status: 200 }))
-    .catch(() => {
-      Error('');
+    .then((resolve) => {
+      resolve({ status: 200, body: 'Success' });
+    })
+    .catch((reject) => {
+      reject(new Error());
     })
     .finally(() => {
       console.log('Got a response from the API');
