0cada3c49f5b987140977f37b4fb368049ad2c57
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-06-05 00:29:23 +0300

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => {
       const list = [];
       for (const res of results) {
         list.push({ status: res.status, value: res.value });
       }
-      console.log(list);
       return list;
     });
 }
