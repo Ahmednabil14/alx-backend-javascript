0e93485e612afb105945b97da47672d6eb18c0d7
ahmednabil14 <ahmednabil14499@gmail.com>
2024-06-14 23:41:06 +0300

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,19 +1,11 @@
 export default function cleanSet(set, value) {
   let str = '';
-  for (const s of set) {
-    let flag = false;
-    for (let i = 0; i < value.length; i += 1) {
-      if (s[i] !== value[i]) {
-        flag = false;
-        break;
-      } else {
-        flag = true;
-      }
-    }
-    if (flag === true) {
-      if (s !== set[set.length - 1]) {
-        str += `${s.slice(value.length)}-`;
-      }
+  if (value === '') {
+    return str;
+  }
+  for (const item of set) {
+    if (item.startsWith(value)) {
+      str += `${item.slice(value.length)}-`;
     }
   }
   return str.slice(0, -1);
