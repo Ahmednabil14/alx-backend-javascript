7b400647c35cea955502e1b9d02a499bedc73e56
ahmednabil14 <ahmednabil14499@gmail.com>
2024-06-14 23:59:12 +0300

    task 9

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,9 @@
 export default function groceriesList() {
   const grocerList = new Map();
-  grocerList.set('Apples', '10');
-  grocerList.set('Tomatoes', '10');
-  grocerList.set('Pasta', '1');
-  grocerList.set('Rice', '1');
-  grocerList.set('Banana', '5');
+  grocerList.set('Apples', 10);
+  grocerList.set('Tomatoes', 10);
+  grocerList.set('Pasta', 1);
+  grocerList.set('Rice', 1);
+  grocerList.set('Banana', 5);
   return grocerList;
 }
